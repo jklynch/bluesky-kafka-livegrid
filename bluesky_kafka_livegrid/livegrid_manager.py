f3d8a126267fec8f6915815ef76cb994bb4e1a1e
Joshua Lynch <jlynch@bnl.gov>
2020-01-24 13:50:56 -0500

    add command line arguments add LiveGrid

--- bluesky_kafka_livegrid/livegrid_manager.py
+++ bluesky_kafka_livegrid/livegrid_manager.py
@@ -1,24 +1,33 @@
+import argparse
 import pprint
 
-import msgpack
+from bluesky.callbacks.mpl_plotting import LiveGrid
 
-from bluesky_kafka.kafka import RemoteDispatcher
+from bluesky_kafka_livegrid.live_server import live_server
 
 
 class LiveGridManager:
     def __init__(self):
-        self.live_plots = dict()
+        self.live_grids = dict()
+        self.descriptor_uid_to_run_uid = dict()
 
     def __call__(self, name, doc):
         if name == "start":
             print(f"starting a LiveGrid for run {doc['uid']}")
-            run_start_uuid = doc["uid"]
+            pprint.pprint(doc)
+            run_start_uid = doc["uid"]
+            self.live_grids[run_start_uid] = LiveGrid(raster_shape=(10, 10), I="det")
+            self.live_grids[run_start_uid].start(doc)
         elif name == "descriptor":
             print("descriptor:")
             pprint.pprint(doc)
+            self.descriptor_uid_to_run_uid[doc["uid"]] = doc["run_start"]
         elif name == "event":
             print("event:")
             pprint.pprint(doc)
+            run_uid = self.descriptor_uid_to_run_uid[doc["descriptor"]]
+            livegrid = self.live_grids[run_uid]
+            livegrid.event(doc)
         elif name == "stop":
             print("stop:")
             pprint.pprint(doc)
@@ -26,20 +35,15 @@ class LiveGridManager:
             print(name)
 
 
-def livegrid_server():
-    dispatcher = RemoteDispatcher(
-        topics=["srx-bluesky"],
-        bootstrap_servers="10.5.0.18:9092",
-        group_id="livegrid",
-        #deserializer=msgpack.unpackb,
-    )
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("topics")
+    argparser.add_argument("bootstrap-servers")
+    argparser.add_argument("group-id")
+
+    args = argparser.parse_args()
+    print(args)
 
     manager = LiveGridManager()
 
-    dispatcher.subscribe(func=manager)
-
-    dispatcher.start()
-
-
-if __name__ == "__main__":
-    livegrid_server()
+    live_server(manager=manager, **vars(args))
