e2a4942208527bd12fd2993e0f264fde1645bba4
xf28id1 <none>
2020-02-05 17:45:30 -0500

    update comment

--- bluesky_kafka_livegrid/live_server.py
+++ bluesky_kafka_livegrid/live_server.py
@@ -3,7 +3,7 @@ from bluesky_kafka.kafka import RemoteDispatcher
 
 def live_server(manager, topics, bootstrap_servers, group_id):
     dispatcher = RemoteDispatcher(
-        topics=topics,                          # ["srx-bluesky"],
+        topics=topics,                          # ["srx.bluesky.documents"],
         bootstrap_servers=bootstrap_servers,    # "10.5.0.18:9092",
         group_id=group_id,                      # "livegrid",
         #deserializer=msgpack.unpackb,
