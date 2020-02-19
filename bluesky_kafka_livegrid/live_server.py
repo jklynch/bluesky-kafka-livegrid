1bd316f9e243e005278a9ac9404b32eaa03cb73f
Joshua Lynch <jlynch@bnl.gov>
2020-02-19 10:40:18 -0500

    update module import

--- bluesky_kafka_livegrid/live_server.py
+++ bluesky_kafka_livegrid/live_server.py
@@ -1,12 +1,12 @@
-from bluesky_kafka.kafka import RemoteDispatcher
+from bluesky_kafka import RemoteDispatcher
 
 
 def live_server(manager, topics, bootstrap_servers, group_id):
     dispatcher = RemoteDispatcher(
-        topics=topics,                          # ["srx.bluesky.documents"],
-        bootstrap_servers=bootstrap_servers,    # "10.5.0.18:9092",
-        group_id=group_id,                      # "livegrid",
-        #deserializer=msgpack.unpackb,
+        topics=topics,  # ["srx.bluesky.documents"],
+        bootstrap_servers=bootstrap_servers,  # "10.5.0.18:9092",
+        group_id=group_id,  # "livegrid",
+        # deserializer=msgpack.unpackb,
     )
 
     dispatcher.subscribe(func=manager)
